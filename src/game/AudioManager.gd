1d6e7ef44ae2f7f9a7bdffa8fda5817a4adef057
Luke Quinlan <lukeq1000@gmail.com>
2021-08-08 15:41:36 +0100

    Add Basic Play/Draw Sound Effects

---
+++ src/game/AudioManager.gd
@@ -0,0 +1,21 @@
+extends AudioStreamPlayer
+
+onready var draw_card = load("res://assets/sound_effects/draw.wav")
+onready var play_card = load("res://assets/sound_effects/play.wav")
+
+func _ready():
+	Server.connect("cards_drawn", self, "_on_cards_drawn")
+	Server.connect("card_played", self, "_on_card_played")
+
+func _on_cards_drawn(player, cards):
+	set_stream(draw_card)
+	_play()
+
+func _on_card_played(player, card):
+	set_stream(play_card)
+	_play()
+
+func _play():
+	volume_db = 1
+	pitch_scale = 1
+	play()
