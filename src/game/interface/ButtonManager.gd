7a929a15da5e576c3e353eecb030e460f814ac6f
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2021-08-08 16:55:34 +0100

    Add double ripple effect when a card can be drawn

--- src/game/interface/ButtonManager.gd
+++ src/game/interface/ButtonManager.gd
@@ -6,15 +6,21 @@ onready var wild_picker = get_node("WildPicker")
 onready var top_deck = get_node("../PlaySpace/Deck/TopDeckLayer/TopDeckLayer2/TopDeckCard2")
 onready var tween = get_node("Draw/Tween")
 onready var ripple_anim = get_node("Draw/Ripple")
+onready var ripple_anim2 = get_node("Draw/Ripple2")
+onready var highlight1 = get_node("Draw/Highlight")
+onready var highlight2 = get_node("Draw/Highlight2")
 
 var draw_card_position
+var ripple_playing = false
 
 func _ready():
 	Server.connect("wild_pick_request", self, "_on_wild_pick_request")
-	draw.hide()
+	# draw.hide()
 	uno.hide()
 	wild_picker.hide()
 	draw_card_position = top_deck.rect_position
+	highlight1.hide()
+	highlight2.hide()
 	play_ripple_anim()
 
 func enable_draw_button(enable: bool):
@@ -22,7 +28,7 @@ func enable_draw_button(enable: bool):
 		draw.show()
 	else:
 		reset_top_deck_position()
-		draw.hide()
+		# draw.hide()
 
 func enable_uno_button(enable: bool):
 	if enable:
@@ -70,7 +76,18 @@ func reset_top_deck_position():
 	tween.start()
 
 func play_ripple_anim():
+	highlight1.show()
+	ripple_playing = true
 	ripple_anim.play("DrawRipple")
+	yield(get_tree().create_timer(1.5), "timeout")
+	if ripple_playing:
+		ripple_anim2.play("DrawRipple2")
+		yield(get_tree().create_timer(0.75), "timeout")
+		highlight2.show()
 
 func stop_ripple_anim():
+	ripple_playing = false
 	ripple_anim.stop()
+	ripple_anim2.stop()
+	highlight1.hide()
+	highlight2.hide()
